1e4d4d7ccbe5e107c80fae3005b2e08effd49d42
maw03 <mats.wintersto@hotmail.no>
2026-01-14 02:01:19 +0100

    Add Game component and basic layout styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Game from "./Game";
 
 function App() {
-
   return (
-    <div>
-      {/* comment  */}
+    <div className="app">
+      <Game />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
