182e7830641844ddd5a736762f88dbb66d179a38
Mats Amundsen Winterstø <158567389+MatsAW03@users.noreply.github.com>
2026-01-23 01:42:39 +0100

    Refactor tile feedback to use shared evaluateGuess helper

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,41 +1,22 @@
 import "./Row.css";
 import React from "react";
 import { WORD_LENGTH } from "./constants";
+import { evaluateGuess } from "./utils/evaluateGuess";
 
 function Row({ guess, isFinal, solution }) {
   const tiles = [];
 
-  let statuses = Array(WORD_LENGTH).fill("incorrect");
-
-  if (isFinal) {
-    const solutionChars = solution.split("");
-    const guessChars = guess.split("");
-    const remaining = {};
-
-    for (let i = 0; i < WORD_LENGTH; i++) {
-      if (guessChars[i] === solutionChars[i]) {
-        statuses[i] = "correct";
-      } else {
-        const c = solutionChars[i];
-        remaining[c] = (remaining[c] || 0) + 1;
-      }
-    }
-
-    for (let i = 0; i < WORD_LENGTH; i++) {
-      if (statuses[i] === "correct") {
-        continue;
-      }
-      const c = guessChars[i];
-      if (remaining[c] > 0) {
-        statuses[i] = "misplaced";
-        remaining[c]--;
-      }
-    }
-  }
+  const statuses = isFinal
+    ? evaluateGuess(guess, solution)
+    : Array(WORD_LENGTH).fill("");
 
   for (let i = 0; i < WORD_LENGTH; i++) {
-    const char = guess[i];
-    let className = `tile ${isFinal ? statuses[i] : ""}`.trim();
+    const char = guess[i] ?? "";
+    let className = "tile";
+
+    if (isFinal) {
+      className += ` ${statuses[i]}`;
+    }
 
     tiles.push(
       <div key={i} className={className}>
---
+++ src/utils/evaluateGuess.jsx
@@ -0,0 +1,25 @@
+export function evaluateGuess(guess, solution) {
+  const statuses = Array(guess.length).fill("incorrect");
+  const remaining = {};
+
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === solution[i]) {
+      statuses[i] = "correct";
+    } else {
+      const c = solution[i];
+      remaining[c] = (remaining[c] || 0) + 1;
+    }
+  }
+
+  for (let i = 0; i < guess.length; i++) {
+    if (statuses[i] === "correct") {
+      continue;
+    }
+    const c = guess[i];
+    if (remaining[c] > 0) {
+      statuses[i] = "misplaced";
+      remaining[c]--;
+    }
+  }
+  return statuses;
+}
