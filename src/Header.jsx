45dd7e0baad802958506db6e5b78dde452665ec3
MatsAW03 <158567389+MatsAW03@users.noreply.github.com>
2026-02-09 02:57:10 +0100

    Add Header component with Wordle title

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="app-header">
+      <div className="title">WORDLE</div>
+    </header>
+  );
+}
+
+export default Header;
